bf9cc325361924c6edb4884707406a30376bf704
Josh Imbriani <joshimbriani@gmail.com>
2015-02-09 14:17:25 -0600

    Added a simple file uploader with EXIF support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({dest: './uploads/'}));
 
 app.use('/', routes);
 app.use('/api/v1', api);
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var exif = require('exif').ExifImage;
 
-/* GET home page. */
-router.get('/', function(req, res) {
-	  res.send('test');
+router.post('/photo', function(req, res) {
+	try {
+		new exif({image:req.files.fileupload.path}, function(error, exifData) {
+			if (error) {
+				console.log('Error: ' + error.message);
+			} else {
+				console.log(exifData);
+			}
+		});
+	} catch (error) {
+		console.log('Error: ' + error.message);
+	}
+	res.redirect('/');
 });
 
 module.exports = router;
